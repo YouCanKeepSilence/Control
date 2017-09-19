0417c2e5674feacc55f0269e831a6bcde117a89b
Silence <youcankeepsilence@gmail.com>
2017-09-19 14:57:01 +0300

    working

--- routes/control_routes.js
+++ routes/control_routes.js
@@ -1,14 +1,29 @@
 module.exports = function(app, db) {
     app.post('/control', (req,res) => {
-
+        console.log('biba');
         var details = { 'name' : 'Tom' };
-
-        res.send(db.collection('users').findOne(details, (err, item) => {
-            if (err) {
-                return console.log(err);
-            }
+        //res.send("Superrrrr");
+        var collection = db.collection("users");
+        res.send(collection.insertOne(details, function(err, result){
             
-            res.send(item);
-        }));
+           if(err){ 
+               return console.log(err);
+           }
+           console.log(result.ops);
+            }));
+
+
+    app.get('/ziga' , (req,res) => {
+        console.log('biba');
+        var details = { 'name' : 'Jerry', 'nation' : 'nigga' };
+        //res.send("Superrrrr");
+        var collection = db.collection("users");
+        res.send(collection.insertOne(details, function(err, result){
+            
+           if(err){ 
+               return console.log(err);
+           }
+           console.log(result.ops);
+            }))});
     });
 };
