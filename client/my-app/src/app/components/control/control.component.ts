313517bf644e6eae4a55109af88f89938d324601
Befezdow <befezd@gmail.com>
2017-09-21 00:29:02 +0400

    cant connect app

---
+++ client/my-app/src/app/components/control/control.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'control',
+    templateUrl: 'control.component.html',
+})
+export class ControlComponent { }
